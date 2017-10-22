eea21eccd841dc7281b04bb6089dc6941b819c37
Fortunat Mutunda <fotimutunda@gmail.com>
2017-10-22 03:40:58 +0300

    get a certain date rage per week at once

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const express = require('express'),
     bodyParser = require('body-parser'),
     cors = require('cors'),
     axios = require('axios'),
-    moment = require('moment');
+    moment = require('moment'),
+    _ = require('lodash');
 
 //
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT =  5000;
 app.set('port', (process.env.PORT || 5000));
 
 app.use(bodyParser.json());
@@ -34,13 +35,43 @@ app.get('/range', function (req, res) {
                 console.log(error)
             })
     }else{
-        var keys = Object.keys(req.query);
-        console.log(keys);
-        res.send(req.query)
+        const base = req.query.base;
+        let startDate = req.query.startDate;
+        const target = req.query.target;
+        const endDate = req.query.endDate;
+        const chunk = {};
+        const promises = []
+        const diff = moment(startDate).diff(endDate, 'weeks');
+
+        res.setHeader('content-type', 'application/json');
+
+        for (let i = 1; i <= diff; i ++){
+            // if(moment(startDate).diff(endDate, 'weeks') >= 0 ){
+
+                promises.push(axios.get('https://api.fixer.io/'+ startDate + '?' +'base='+ base + '&' + 'symbols=' + target ));
+                startDate = ( moment(startDate).add(-i, 'weeks').format('YYYY-MM-DD'))
+            // }
+
+        }
+
+        axios.all(promises)
+            .then(function (responses) {
+                responses.forEach(function (response, i) {
+                    chunk[i+''] += response.data
+                    console.log(response.data)
+                    // res.write(response.data)
+                })
+            }).catch(function (erros) {
+            res.json(erros)
+        });
+        console.log(chunk, 'chunkiiii')
+        res.json(chunk)
+        res.end()
+
+
     }
 
-})
-
+});
 
 app.listen(app.get('port'), function() {
     console.log('http://localhost:%s  and waiting for requests', PORT);
